2444f91c69e51b3d1e7ce714a39cfefe783bcbb8
bang-ibe <85978429+bang-ibe@users.noreply.github.com>
2021-06-21 16:59:46 +0700

    Create Intro_and_Variable_Types.py

---
+++ Intro_and_Variable_Types.py
@@ -0,0 +1,91 @@
+# Basic Syntax
+print("Hello World")
+print("Hello \nWorld!")
+
+# Quotation in Python
+a = 'Python'
+b = " Programming"
+c = """ is so freaking easy"""
+sentence = a+b+c
+print(sentence)
+
+# Multiple Statement in a single line
+x = "5"; y = " balon"; print(x+y)
+
+# Parameters
+tipe_data = ["Boolean","Float","Integer","String"] 
+print(tipe_data)
+tipe_data = '\n'.join(tipe_data)
+print("List setelah menggunakan .join untuk setiap item list:\n", tipe_data)
+
+# Data Types
+nama = "ibe"
+type(nama)
+suhu = -99
+type(suhu)
+suhu_matahari = 1000000000
+type(suhu_matahari)
+suhu_kutub = -4.6
+type(suhu_kutub)
+ibe_ganteng = TRUE
+type(ibe_ganteng)
+ibe_jelek = FALSE
+type(ibe_jelek)
+
+# Assigning Value to Variable
+iphone7 = 2500000 # Automatically Integer
+Laptop = input("Masukkan harga laptop:") # Automatically String even the value is number
+  # >
+ type(Laptop)
+Sensors = int(input("Masukkan harga sensor-sensor: ")
+              # >
+type(Sensors)
+mouse = 125000; type(mouse)
+headphone = 250000.0; type(headphone)
+Kopi_SBUX = "Mahal Banget Ngab"; type(kopi_SBUX)
+
+# Multiple Assignment
+a,b,c = 69,68,67
+x = y = z = 38000
+
+# Standart Data Types
+# There are several data types: Number, String, List, Tuple, Dictionary
+# Number Consist of int, float, and complex
+              
+# Python String
+sedotan = "Panjang Banget huehuehue "
+print(sedotan)  # Print complete List / sentence
+print(sedotan[0]) # Print 1st index of List (1st char)
+print(sedotan[4:9])
+print(sedotan[7:])
+print(sedotan[:12])
+print(sedotan[-1])
+print(sedotan + "dan bahannya stainless skill xixixi"
+print(sedotan*2)
+
+print("Nama gue %s umur gue %d" % ("siapa dah", 23)
+
+      
+# Python List
+# Python Lists are similar to arrays in C programming
+# all items can be different data type and can be updated
+contoh_list = ["bisa string", 20, 90.0, True,'john', 'mayer']
+print(contoh_list)
+print(contoh_list[0]) # Print first list
+print(contoh_list[2:5])
+print(contoh_list[:4])
+print(contoh_list[2:])
+print(contoh_list*2)
+print(contoh_list[-1])
+      
+# Python Tuple
+# Just like list but cannot be update/changed
+Contoh_Tuple  = ("Contoh tuple", 20, 9.0, False)
+print(Contoh_Tuple)
+# sisanya kayak list dan string, gue males ngetiknya hehe
+
+      
+             
+              
+
+
