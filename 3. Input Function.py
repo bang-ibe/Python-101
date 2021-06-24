bdb2b01260a04efad5f31712e7f3fcd114da969a
bang-ibe <85978429+bang-ibe@users.noreply.github.com>
2021-06-24 14:16:21 +0700

    Create 3. Input Function.py

---
+++ 3. Input Function.py
@@ -0,0 +1,38 @@
+input("nama bapak: ")
+# >
+nama_bapak = input("nama bapak: ")
+#>
+print(nama_bapak)
+print("halo", nama_bapak)
+print("halo pak" + nama_bapak +"!")
+
+
+nama_mobil = input("Nama Mobil: ")
+# >
+perusahaan = input("Perusahaan: ")
+# >
+print("cetak informasi")
+print("Nama mobil:", nama_mobil)
+print("Perusahaan:", perusahaan)
+
+
+# number
+number = input("Masukkan angka: ")
+# >
+print("Number", number, "Tipe data: "); type(number)
+real_number = int(input("masukkan angka: "))
+# >
+print("Angka:", real_number, "tipe data:"); type(real_number)
+
+
+# Kalkulator sederhana
+angka1 = float(input("masukkan angka 1: "))
+# >
+angka2 = int(input("masukkan angka 2: "))
+# >
+print("angka 1: ", angka1)
+print("angka 2: ", angka2)
+sum = angka1 + angka2
+print("hasil penjumlahan angka 1 + angka 2: ", sum) 
+# Don't need to change the data type bcs the result would be float
+
